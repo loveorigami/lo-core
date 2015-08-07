2d6b699880ffc23721b5fdd1c081a40548ea68cd
Loveorigami <loveorigami@mail.ru>
2015-08-07 11:05:47 +0300

    inputs

---
+++ inputs/DropDownInput.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace lo\core\inputs;
+
+use lo\core\widgets\DependDropDown;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+
+/**
+ * Class DropDownInput
+ * Выпадающий список
+ * @package lo\core\inputs
+ * @author Churkin Anton <webadmin87@gmail.com>
+ */
+class DropDownInput extends BaseInput {
+
+    /**
+     * Формирование Html кода поля для вывода в форме
+     * @param ActiveForm $form объект форма
+     * @param array $options массив html атрибутов поля
+     * @param bool|int $index инднкс модели при табличном вводе
+     * @return string
+     */
+    public function renderInput(ActiveForm $form, Array $options = [], $index = false)
+    {
+        $options = ArrayHelper::merge($this->options, $options);
+
+        $widgetOptions = ArrayHelper::merge(
+            ["options"=>["class" => "form-control", "prompt"=>""], "data"=>$this->modelField->getDataValue()],
+            $this->widgetOptions,
+            ["options"=>$options]
+        );
+
+        $attr = $this->modelField->attr;
+
+        return $form->field($this->modelField->model, $this->getFormAttrName($index, $attr))->widget(DependDropDown::className(), $widgetOptions);
+    }
+
+
+} 
