817d2dbc5850893ce46df91a38d9a3e13461d44f
Loveorigami <loveorigami@mail.ru>
2015-11-23 13:54:24 +0300

    eav fields

---
+++ db/fields/EavField.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace lo\core\db\fields;
+
+use yii\helpers\ArrayHelper;
+
+/**
+ * Class HtmlField
+ * Поле WYSIWYG редактора. Использует CKEditor
+ * @package lo\core\db\fields
+ * @author Churkin Anton <webadmin87@gmail.com>
+ */
+class EavField extends Field
+{
+    public $showInGrid = false;
+    public $showInFilter = false;
+    public $isRequired = false;
+    public $editInGrid = false;
+    public $showInExtendedFilter = false;
+
+    /**
+     * @inheritdoc
+     */
+
+    public $inputClass = '\lo\core\inputs\EavInput';
+
+    /**
+     * Преффикс поведения
+     */
+    const BEHAVIOR_PREF = "eav";
+
+    /**
+     * @var array настройки поведения
+     */
+
+    public $eavOptions = [];
+    /**
+     * @inheritdoc
+     */
+
+    public function behaviors()
+    {
+        $parent = parent::behaviors();
+
+            $code = self::BEHAVIOR_PREF . ucfirst($this->attr);
+            $parent[$code] = ArrayHelper::merge([
+                'class' => \mirocow\eav\EavBehavior::className(),
+                'valueClass' => \mirocow\eav\models\EavAttributeValue::className(),
+            ], $this->eavOptions);
+
+        return $parent;
+    }
+
+    protected function view()
+    {
+        $view = parent::view();
+        $view["format"] = 'html';
+        $view["value"] = $this->getStringValue($this->model);
+        return $view;
+    }
+
+    protected function getStringValue($model)
+    {
+        $arr = [];
+        foreach ($model->getEavAttributes()->all() AS $attr) {
+            $arr[] = $model[$attr->name];
+        }
+
+        return implode("<br>", $arr);
+
+    }
+
+}
---
+++ inputs/EavInput.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace lo\core\inputs;
+
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+use mirocow\eav\widgets\ActiveField;
+
+/**
+ * Class HtmlInput
+ * Html поле
+ * @package lo\core\inputs
+ * @author Churkin Anton <webadmin87@gmail.com>
+ */
+class EavInput extends BaseInput {
+
+    /**
+     * Формирование Html кода поля для вывода в форме
+     * @param ActiveForm $form объект форма
+     * @param array $options массив html атрибутов поля
+     * @param bool|int $index инднкс модели при табличном вводе
+     * @return string
+     */
+    public function renderInput(ActiveForm $form, Array $options = [], $index = false)
+    {
+        $field = '';
+        foreach($this->modelField->model->getEavAttributes()->all() as $attr){
+            $field.= $form->field($this->modelField->model, $attr->name, ['class' => '\mirocow\eav\widgets\ActiveField'])->eavInput();
+        }
+/*        foreach($this->modelField->model->getEavAttributes()->all() as $attr){
+            $field.= $this->modelField->model[$attr->name];
+        }*/
+        return $field;
+    }
+
+
+} 
