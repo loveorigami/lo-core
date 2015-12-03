c9681673c920f47b8cbc72ea1d461de2082a9369
Loveorigami <loveorigami@mail.ru>
2015-12-03 13:36:09 +0300

    theme support

---
+++ components/Theme.php
@@ -0,0 +1,45 @@
+<?php
+namespace lo\core\components;
+
+use Yii;
+use yii\helpers\FileHelper;
+
+class Theme extends \yii\base\Theme {
+    public $active;
+
+    /**
+     * @inheritdoc
+     * https://github.com/sheillendra/yii2-theme
+     */
+
+    public function applyTo($path)
+    {
+        if($this->active){
+            $pathMap = $this->pathMap[$this->active];
+        }else{
+            $pathMap = $this->pathMap;
+            if (empty($pathMap)) {
+                if (($basePath = $this->getBasePath()) === null) {
+                    throw new InvalidConfigException('The "basePath" property must be set.');
+                }
+                $pathMap = [Yii::$app->getBasePath() => [$basePath]];
+            }
+        }
+
+        $path = FileHelper::normalizePath($path);
+        foreach ($pathMap as $from => $tos) {
+            $from = FileHelper::normalizePath(Yii::getAlias($from)) . DIRECTORY_SEPARATOR;
+            if (strpos($path, $from) === 0) {
+                $n = strlen($from);
+                foreach ((array) $tos as $to) {
+                    $to = FileHelper::normalizePath(Yii::getAlias($to)) . DIRECTORY_SEPARATOR;
+                    $file = $to . substr($path, $n);
+                    if (is_file($file)) {
+                        return $file;
+                    }
+                }
+            }
+        }
+        return $path;
+    }
+}
