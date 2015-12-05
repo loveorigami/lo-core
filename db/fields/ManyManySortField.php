34f95712e39e8449ff0ee7bc9e93cfc588bacfa3
Loveorigami <loveorigami@mail.ru>
2015-12-05 19:09:34 +0300

    many-many saver

---
+++ db/fields/ManyManySortField.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace lo\core\db\fields;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+
+/**
+ * Class ManyManySortField
+ * Поле для связи ManyMany с возможностью сортировки привязанных элементов
+ * @package lo\core\db\fields
+ * @author Churkin Anton <webadmin87@gmail.com>
+ */
+class ManyManySortField extends ManyManyField
+{
+
+   public $inputClass = "\\lo\core\\inputs\\SortedTagsInput";
+
+} 
---
+++ inputs/SortedTagsInput.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace lo\core\inputs;
+
+use lo\core\widgets\SortedTags;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+
+/**
+ * Class SortedTagsInput
+ * Список тегов с возможностью сортировки
+ * @package lo\core\inputs
+ * @author Churkin Anton <webadmin87@gmail.com>
+ */
+class SortedTagsInput extends BaseInput {
+
+    /**
+     * Формирование Html кода поля для вывода в форме
+     * @param ActiveForm $form объект форма
+     * @param array $options массив html атрибутов поля
+     * @param bool|int $index инднкс модели при табличном вводе
+     * @return string
+     */
+    public function renderInput(ActiveForm $form, Array $options = [], $index = false)
+    {
+        $data = $this->modelField->getDataValue();
+
+        if(empty($data))
+            return false;
+
+        $options = ArrayHelper::merge(["class"=>"form-control"], $this->options, $options, ["multiple"=>true]);
+
+        $widgetOptions = ArrayHelper::merge([
+            "items" => $data,
+            "bootstrap"=>true,
+            "settings" => [
+                    "tokenSeparators" => [',', ' '],
+                ]
+            ], $this->widgetOptions, ["options"=>$options]);
+
+        $attr = $this->getFormAttrName($index, $this->modelField->attr);
+
+        return $form->field($this->modelField->model, $attr)->widget(SortedTags::className(), $widgetOptions);
+    }
+
+
+} 
