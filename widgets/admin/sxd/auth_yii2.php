7809607204b31fd9f18abc58a0e2c9ac93f1c3ab
Loveorigami <loveorigami@mail.ru>
2016-04-11 12:32:05 +0300

    ычв config

--- widgets/admin/sxd/auth_yii2.php
+++ widgets/admin/sxd/auth_yii2.php
@@ -11,10 +11,10 @@ require(__DIR__ . '/../../../../common/config/bootstrap.php');
 require(__DIR__ . '/../../../config/bootstrap.php');
 
 $config = \yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../../../common/config/main.php'),
-	require(__DIR__ . '/../../../../common/config/main-local.php'),
-    require(__DIR__ . '/../../../config/main.php'),
-	require(__DIR__ . '/../../../config/main-local.php')
+    require(__DIR__ . '/../../../../common/config/main.global.php'),
+	require(__DIR__ . '/../../../../common/config/main.local.php'),
+    require(__DIR__ . '/../../../config/main.global.php'),
+	require(__DIR__ . '/../../../config/main.local.php')
 );
 
 $application = new \yii\web\Application($config);
