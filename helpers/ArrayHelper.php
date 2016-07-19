c98012a98550e92e64bd3aacaea12df5dcac71ef
Loveorigami <loveorigami@mail.ru>
2016-07-19 11:54:09 +0300

    mb_ucfirst

--- helpers/ArrayHelper.php
+++ helpers/ArrayHelper.php
@@ -16,50 +16,53 @@ class ArrayHelper extends YiiArrayHelper
     /**
      * Трансформирует nested-set массив к массиву с вложенными ветками
      *
-    [
-        [
-            [id] => 1
-            [level] => 1
-            [lft] => 1
-            [rgt] => 8
-            [name] => parent
-        ],
-        [
-            [id] => 2
-            [level] => 2
-            [lft] => 2
-            [rgt] => 7
-            [name] => node
-        ]
-    ]
+     * Исходный массив
+     * ```php
+     *  [
+     *      [
+     *          [id] => 1
+     *          [level] => 1
+     *          [lft] => 1
+     *          [rgt] => 8
+     *          [name] => parent
+     *      ],
+     *      [
+     *          [id] => 2
+     *          [level] => 2
+     *          [lft] => 2
+     *          [rgt] => 7
+     *          [name] => node
+     *      ]
+     *  ]
+     *  ```
      *
-    [
-        [
-            [id] => 1
-            [level] => 1
-            [lft] => 1
-            [rgt] => 8
-            [name] => parent
-            [items] => [
-                [
-                    [id] => 2
-                    [level] => 2
-                    [lft] => 2
-                    [rgt] => 7
-                    [name] => node
-                ]
-            ]
-        ]
-    ]
-     *
+     *  Результирующий массив
+     * ```php
+     *  [
+     *      [
+     *          [id] => 1
+     *          [level] => 1
+     *          [lft] => 1
+     *          [rgt] => 8
+     *          [name] => parent
+     *          [items] => [
+     *              [
+     *                  [id] => 2
+     *                  [level] => 2
+     *                  [lft] => 2
+     *                  [rgt] => 7
+     *                  [name] => node
+     *              ]
+     *          ]
+     *      ]
+     *  ]
+     * ```
      * @param array
      * @return array
      */
-
-    public static function nestedToNodes($nested_array){
-
+    public static function nestedToNodes($nested_array)
+    {
         $result = [];
-        $level = 0;
         $stack = [];
 
         foreach ($nested_array as $node) {
@@ -88,15 +91,34 @@ class ArrayHelper extends YiiArrayHelper
         return $tree;
     }
 
-    public static function multi_order($arr){
-        $ar2=[];
-        $i=0;
-        $n=5;
-        foreach($arr as $a){
-            $ar2[$i] =(isset($a['pos'])) ? $a['pos'] : $n++;
+    /**
+     * @param $arr
+     * @return mixed
+     */
+    public static function multi_order($arr)
+    {
+        $ar2 = [];
+        $i = 0;
+        $n = 5;
+        foreach ($arr as $a) {
+            $ar2[$i] = (isset($a['pos'])) ? $a['pos'] : $n++;
             $i++;
         }
         array_multisort($arr, SORT_NUMERIC, $ar2);
-        return($arr);
+        return ($arr);
+    }
+
+    /**
+     * mb_ucfirst - make a string's first character uppercase
+     * @param $str - the input string.
+     * @param string $encoding - string $encoding [optional] &mbstring.encoding.parameter; default UTF-8
+     * @return string - string str with first alphabetic character converted to uppercase.
+     */
+    public static function mb_ucfirst($str, $encoding = 'UTF-8')
+    {
+        $str = mb_ereg_replace('^[\ ]+', '', $str);
+        $str = mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding) .
+            mb_substr($str, 1, mb_strlen($str), $encoding);
+        return $str;
     }
 }
