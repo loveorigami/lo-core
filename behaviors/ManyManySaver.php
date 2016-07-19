8c03cda0105aa9ec5d5806254aa30fe62a1b2cff
Loveorigami <loveorigami@mail.ru>
2016-07-19 10:05:44 +0300

    Many saver

--- behaviors/ManyManySaver.php
+++ behaviors/ManyManySaver.php
@@ -2,19 +2,30 @@
 
 namespace lo\core\behaviors;
 
-use yii\base\Behavior;
-use yii\db\ActiveQuery;
-use yii\db\ActiveRecord;
+use voskobovich\behaviors\ManyToManyBehavior;
 
 /**
  * Class ManyManySaver
- * Поведение для сохранения связанных через MANY MANY записей
+ * Поведение для сохранения связанных через ONE-MANY или MANY-MANY записей
  * @package lo\core\behaviors
  *
+ * As an example, let's assume you are dealing with entities like Book, Author and Review. The Book model has the following relationships:
+ *
+ * ```php
+ *  public function getAuthors()
+ *  {
+ *      return $this->hasMany(Author::className(), ['id' => 'author_id'])
+ *      ->viaTable('book_has_author', ['book_id' => 'id']);
+ *  }
+ *
+ *  public function getReviews()
+ *  {
+ *      return $this->hasMany(Review::className(), ['id' => 'review_id']);
+ *  }
+ * ```
+ * In the same model, the behaviour can be configured like so:
  *```php
  *  use lo\core\behaviors\ManyManySaver;
- *
- *  protected $_categoriesIds;
  *
  *  public function behaviors()
  *  {
@@ -22,145 +33,17 @@ use yii\db\ActiveRecord;
  *
  *      $arr["many-cats"] = [
  *          'class' => ManyManySaver::class,
- *          'names' => ['categories'] // relation getCategories()
+ *          'relations' => [
+ *              'author_ids' => 'authors',
+ *              'review_ids' => 'reviews',
+ *          ],
  *      ];
  *
  *      return $arr;
  *  }
  * ```
- *
- * Заполняем данными идентификаторы связвнных категорий
- *
- * ```php
- *  public function getCategoriesIds()
- *  {
- *      if (!is_array($this->_categoriesIds) AND !$this->isNewRecord) {
- *          $this->_categoriesIds = $this->getManyManyIds("categories"); // relation getCategories
- *      }
- *
- *      return $this->_categoriesIds;
- *  }
- *
- *  public function setCategoriesIds($categoriesIds)
- *  {
- *      $this->_categoriesIds = $categoriesIds;
- *  }
- * ```
- *
- *  Связь через промежуточную таблицу
- *
- * ```php
- *  public function getCategories()
- *  {
- *      return $this->hasMany(Category::class, ['id' => 'cat_id'])->viaTable('{{%category_item}}', ['item_id' => 'id']);
- *  }
- * ```
  */
-class ManyManySaver extends Behavior
+class ManyManySaver extends ManyToManyBehavior
 {
-
-    const ATTR_SUFF = "Ids";
-
-    /**
-     * @var ActiveRecord the owner of this behavior.
-     */
-    public $owner;
-
-    /**
-     * @var array массив имен связей для сохранения
-     */
-    public $names = [];
-
-    /**
-     * @inheritdoc
-     */
-    public function events()
-    {
-        return [
-            ActiveRecord::EVENT_AFTER_INSERT => [$this, "afterSave"],
-            ActiveRecord::EVENT_AFTER_UPDATE => [$this, "afterSave"],
-        ];
-    }
-
-    /**
-     * Сохранение связей
-     */
-    public function afterSave()
-    {
-        /**
-         * @var ActiveQuery $query
-         * @var ActiveRecord $modelClass
-         */
-        $this->owner->setIsNewRecord(false);
-
-        foreach ($this->names AS $name) {
-            $attr = $this->getAttributeName($name);
-
-            if ($this->owner->$attr !== null) {
-
-                $query = $this->owner->{"get" . ucfirst($name)}();
-                $modelClass = $query->modelClass;
-                $related = $query->all();
-
-                foreach ($related AS $rel) {
-                    $this->owner->unlink($name, $rel, true);
-                }
-
-                if (empty($this->owner->$attr))
-                    continue;
-
-                $newRelated = $modelClass::find()->where(["id" => $this->owner->$attr])->all();
-
-                usort($newRelated, function ($val1, $val2) use ($attr) {
-
-                    $key1 = array_search($val1->id, $this->owner->$attr);
-                    $key2 = array_search($val2->id, $this->owner->$attr);
-
-                    if ($key1 > $key2)
-                        return 1;
-                    elseif ($key1 < $key2)
-                        return -1;
-                    else
-                        return 0;
-                });
-
-                foreach ($newRelated as $newRel) {
-                    $this->owner->link($name, $newRel);
-                }
-            }
-        }
-    }
-
-    /**
-     * Возвращает имя атрибута хранящего идентификаторы привязываемых записей
-     * @param string $name имя звязи
-     * @return string
-     */
-    public function getAttributeName($name)
-    {
-        return $name . static::ATTR_SUFF;
-    }
-
-    /**
-     * Возвращает массив идентификаторов связанных записей
-     * @param string $name имя связи
-     * @return array
-     */
-    public function getManyManyIds($name)
-    {
-        $ids = [];
-
-        $pk = $this->owner->getPrimaryKey();
-
-        if (empty($pk))
-            return $ids;
-
-        $models = $this->owner->$name;
-
-        foreach ($models AS $model)
-            $ids[] = $model->id;
-
-        return $ids;
-    }
 
 }
