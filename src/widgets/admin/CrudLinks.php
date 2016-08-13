eb09a7b41f342342684e9edd3802ddacdcbf0713
Loveorigami <loveorigami@mail.ru>
2016-08-13 16:39:46 +0300

    comment access for grid buttons

--- src/widgets/admin/CrudLinks.php
+++ src/widgets/admin/CrudLinks.php
@@ -1,6 +1,8 @@
 <?php
 namespace lo\core\widgets\admin;
 
+use lo\core\db\ActiveRecord;
+use lo\core\rbac\AccessRouteTrait;
 use Yii;
 use yii\base\Widget;
 use yii\helpers\Html;
@@ -13,87 +15,62 @@ use yii\helpers\Html;
  */
 class CrudLinks extends Widget
 {
-    use \lo\core\rbac\AccessRouteTrait;
-    /**
-     * Константы CRUD действий
-     */
+    use AccessRouteTrait;
 
+    /** Константы CRUD действий */
     const CRUD_LIST = "list";
-
     const CRUD_VIEW = "view";
 
-    /**
-     * @var string действие для которого отобразить кнопки (self::CRUD_LIST|self::CRUD_VIEW)
-     */
-
+    /** @var string действие для которого отобразить кнопки (self::CRUD_LIST|self::CRUD_VIEW) */
     public $action;
 
-    /**
-     * @var \lo\core\db\ActiveRecord модель
-     */
-
+    /** @var ActiveRecord модель */
     public $model;
 
-    /**
-     * @var array массив дополнительных параметров для url
-     */
-
+    /** @var array массив дополнительных параметров для url */
     public $urlParams = [];
 
     /**
      * @inheritdoc
      */
-
-
-
     public function run()
     {
-
         $buttons = $this->getButtons()[$this->action];
 
         $html = '';
 
         foreach ($buttons AS $button) {
-
             if (Yii::$app->user->can($button['permission'], ['model' => $this->model]))
                 $html .= Html::a($button["label"], $button["url"], $button["options"]) . "\n ";
-
         }
 
         return $html;
-
     }
 
     /**
      * Возвращает описание ссылок !Yii::$app->user->can($this->baseRoute . '/update', ['model'=>$model])
      * @return array
      */
-
     public function getButtons()
     {
-
         return $buttons = [
 
             self::CRUD_LIST => [
-
                 [
                     'label' => Yii::t('core', 'Create'),
                     'url' => array_merge(['create'], $this->urlParams),
                     'options' => ['class' => 'btn btn-success pull-right'],
                     'permission' => $this->access('create'),
                 ]
-
             ],
 
             self::CRUD_VIEW => [
-
                 [
                     'label' => Yii::t('core', 'Update'),
                     'url' => array_merge(['update', 'id' => $this->model->id], $this->urlParams),
                     'options' => ['class' => 'btn btn-primary  pull-right'],
                     'permission' => $this->access('update'),
                 ],
-
                 [
                     'label' => Yii::t('core', 'Delete'),
                     'url' => array_merge(['delete', 'id' => $this->model->id], $this->urlParams),
@@ -105,11 +82,7 @@ class CrudLinks extends Widget
                     ],
                     'permission' => $this->access('delete'),
                 ],
-
             ],
-
         ];
-
     }
-
 }
--- src/widgets/admin/Grid.php
+++ src/widgets/admin/Grid.php
@@ -1,8 +1,11 @@
 <?php
 namespace lo\core\widgets\admin;
 
+use lo\core\db\ActiveRecord;
+use lo\core\rbac\AccessRouteTrait;
 use Yii;
 use yii\base\Widget;
+use yii\data\ActiveDataProvider;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -18,68 +21,42 @@ use yii\helpers\Url;
  */
 class Grid extends Widget
 {
+    use AccessRouteTrait;
 
-    use \lo\core\rbac\AccessRouteTrait;
-
-    /**
-     * Преффикс идентификатора грида
-     */
+    /** Преффикс идентификатора грида */
     const GRID_ID_PREF = "grid-";
 
-    /**
-     * Суфикс иденификатора виджета Pjax
-     */
+    /** Суфикс иденификатора виджета Pjax */
     const PJAX_SUF = "-pjax";
 
-    /**
-     * @var \lo\core\db\ActiveRecord модель
-     */
+    /** @var ActiveRecord модель */
     public $model;
 
-    /**
-     * @var \yii\data\ActiveDataProvider провайдер данных
-     */
+    /** @var ActiveDataProvider провайдер данных */
     public $dataProvider;
 
-    /**
-     * @var string имя параметра передаваемого расширенным фильтром
-     */
+    /** @var string имя параметра передаваемого расширенным фильтром */
     public $extFilterParam = "extendedFilter";
 
-    /**
-     * @var array кнопки строк грида
-     */
+    /** @var array кнопки строк грида */
     protected $_rowButtons;
 
-    /**
-     * @var array дополнительные пользовательские колонки
-     */
+    /**@var array дополнительные пользовательские колонки */
     public $userColumns = [];
 
-    /**
-     * @var bool вывод древовидных моделей
-     */
+    /** @var bool вывод древовидных моделей */
     public $tree = false;
 
-    /**
-     * @var string шаблон
-     */
+    /** @var string шаблон */
     public $tpl = "grid";
 
-    /**
-     * @var array кнопки групповых операций
-     */
+    /** @var array кнопки групповых операций */
     protected $_groupButtons;
 
-    /**
-     * @var string идентификатор виджета
-     */
-
+    /** @var string идентификатор виджета */
     protected $id;
-    /**
-     * @var string идентификатор виджета PJAX
-     */
 
+    /** @var string идентификатор виджета PJAX */
     protected $pjaxId;
 
     /**
@@ -88,9 +65,7 @@ class Grid extends Widget
     public function getRowButtons()
     {
         if ($this->_rowButtons === null) {
-
             $this->_rowButtons = $this->defaultRowButtons();
-
         }
 
         return $this->_rowButtons;
@@ -107,25 +82,18 @@ class Grid extends Widget
 
     public function init()
     {
-
         $model = $this->model;
-
         $this->id = strtolower(self::GRID_ID_PREF . str_replace("\\", "-", $model::className()));
-
         $this->pjaxId = $this->id . self::PJAX_SUF;
-
         $this->view->registerCss(".grid-checkbox-disabled input[type='checkbox'] { display:none; }");
-
     }
 
     /**
      * Запуск виджета
      * @return string|void
      */
-
     public function run()
     {
-
         return $this->render($this->tpl, [
             "model" => $this->model,
             "dataProvider" => $this->dataProvider,
@@ -134,89 +102,67 @@ class Grid extends Widget
             "id" => $this->id,
             "pjaxId" => $this->pjaxId,
         ]);
-
     }
 
     /**
      * Возвращает описание колонок
      * @return array
      */
-
     protected function getColumns()
     {
-
         $columns = [
-
             [
                 'class' => 'yii\grid\CheckboxColumn',
-                'contentOptions' => function ($model, $key, $index, $gridView) {
+/*                'contentOptions' => function ($model, $key, $index, $gridView) {
                     $arr = [];
-
                     if (
                         !Yii::$app->user->can($this->access('update'), ['model' => $model]) AND
                         !Yii::$app->user->can($this->access('delete'), ['model' => $model])
                     ) {
                         $arr = ["class" => "grid-checkbox-disabled"];
                     }
-
                     return $arr;
-                },
+                },*/
                 'headerOptions' => ['style' => 'width: 30px;']
             ],
-
         ];
 
         $fields = $this->model->getMetaFields()->getFields();
 
         foreach ($fields AS $field) {
-
             $grid = $field->getGrid();
 
             if ($field->showInGrid AND $grid)
                 $columns[] = $grid;
-
         }
 
         $columns = ArrayHelper::merge($columns, $this->userColumns);
-
         $columns[] = $this->getRowButtons();
 
         return $columns;
-
     }
 
     /**
      * Возвращает настройки по умолчанию кнопок действий над моделями
      * @return array
      */
-
     public function defaultRowButtons()
     {
-
         $js = function ($u) {
-
             return '$.get("' . $u . '", function(){ $.pjax.reload({container: "#' . $this->pjaxId . '", timeout: false}); }); return false;';
-
         };
 
         $buttonsTree = [
-
             'up' => function ($url, $model) use ($js) {
-
                 $url = Url::toRoute([$this->baseRoute . '/up', 'id' => $model->id]);
-
-                if (Yii::$app->user->can($this->access('update'), ['model' => $model]))
+                //if (Yii::$app->user->can($this->access('update'), ['model' => $model]))
                     return Html::tag('a', Html::tag('i', '', ['class' => 'glyphicon glyphicon-arrow-up']), ['data-pjax' => 0, 'onClick' => $js($url), 'href' => '#', 'title' => Yii::t('core', 'Up'), 'class' => 'btn btn-xs btn-success']);
-
             },
 
             'down' => function ($url, $model) use ($js) {
-
                 $url = Url::toRoute([$this->baseRoute . '/down', 'id' => $model->id]);
-
-                if (Yii::$app->user->can($this->access('update'), ['model' => $model]))
+                //if (Yii::$app->user->can($this->access('update'), ['model' => $model]))
                     return Html::tag('a', Html::tag('i', '', ['class' => 'glyphicon glyphicon-arrow-down']), ['data-pjax' => 0, 'onClick' => $js($url), 'href' => '#', 'title' => Yii::t('core', 'Down'), 'class' => 'btn btn-xs btn-warning']);
-
             },
 
             'enter' => function ($url, $model) {
@@ -230,43 +176,31 @@ class Grid extends Widget
                         return Html::tag('a', Html::tag('i', '', ['class' => 'glyphicon glyphicon-plus']), ['data-pjax' => 0, 'href' => $url, 'title' => Yii::t('core', 'Create'), 'class' => 'btn btn-xs btn-primary']);
                     }
                 }
-
             },
         ];
 
         $buttonsDefault = [
-
             'view' => function ($url, $model) use ($js) {
-
                 $url = Url::toRoute([$this->baseRoute . '/view', 'id' => $model->id]);
-
-                if (Yii::$app->user->can($this->access('view'), ['model' => $model]))
+                //if (Yii::$app->user->can($this->access('view'), ['model' => $model]))
                     return Html::tag('a', Html::tag('i', '', ['class' => 'glyphicon glyphicon-eye-open']), ['data-pjax' => 0, 'href' => $url, 'title' => Yii::t('core', 'View'), 'class' => 'btn btn-xs btn-primary']);
-
             },
 
             'update' => function ($url, $model) use ($js) {
-
                 $url = Url::toRoute([$this->baseRoute . '/update', 'id' => $model->id]);
-
-                if (Yii::$app->user->can($this->access('update'), ['model' => $model]))
+                //if (Yii::$app->user->can($this->access('update'), ['model' => $model]))
                     return Html::tag('a', Html::tag('i', '', ['class' => 'glyphicon glyphicon-pencil']), ['data-pjax' => 0, 'href' => $url, 'title' => Yii::t('core', 'Update'), 'class' => 'btn btn-xs btn-primary']);
-
             },
 
             'delete' => function ($url, $model) use ($js) {
-
                 $url = Url::toRoute([$this->baseRoute . '/delete', 'id' => $model->id]);
-
-                if (Yii::$app->user->can($this->access('delete'), ['model' => $model]))
+                //if (Yii::$app->user->can($this->access('delete'), ['model' => $model]))
                     return Html::a(Html::tag('i', '', ['class' => 'glyphicon glyphicon-trash']), $url, ['data-pjax' => 0, 'data-method' => 'post', 'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'), 'title' => Yii::t('core', 'Delete'), 'class' => 'btn btn-xs btn-danger']);
-
             },
 
         ];
 
         if ($this->tree) {
-
             return [
                 'class' => 'yii\grid\ActionColumn',
                 'template' => '{enter} {up} {down}<div class="clear_small2"></div>{update} {view} {delete}',
@@ -275,33 +209,24 @@ class Grid extends Widget
             ];
 
         } else {
-
             return [
                 'class' => 'yii\grid\ActionColumn',
                 'buttons' => $buttonsDefault,
                 'headerOptions' => ['style' => 'width: 95px;']
             ];
-
         }
-
     }
 
     /**
      * Возвращает массив кнопок групповых операций
-     * @return arr
+     * @return array
      */
-
     public function getGroupButtons()
     {
-
         if ($this->_groupButtons === null) {
-
             $this->_groupButtons = $this->defaultGroupButtons();
-
         }
-
         return $this->_groupButtons;
-
     }
 
     /**
@@ -317,9 +242,7 @@ class Grid extends Widget
      */
     public function setGroupButtons(Array $buttons)
     {
-
         $this->_groupButtons = ArrayHelper::merge($this->defaultGroupButtons(), $buttons);
-
     }
 
     /**
@@ -329,13 +252,10 @@ class Grid extends Widget
 
     protected function defaultGroupButtons()
     {
-
         $model = $this->model;
-
         $arr = [
-
             "delete" => [
-                "class" => \lo\core\widgets\admin\ActionButton::className(),
+                "class" => ActionButton::class,
                 "label" => Yii::t('core', 'Delete'),
                 "visible" => Yii::$app->user->can($this->access('delete'), ['model' => $model]),
                 "options" => [
@@ -344,14 +264,11 @@ class Grid extends Widget
                 ],
                 "route" => $this->baseRoute . "/groupdelete",
             ],
-
         ];
 
-        if ($this->tree  AND !Yii::$app->request->get($this->extFilterParam)) {
-
+        if ($this->tree AND !Yii::$app->request->get($this->extFilterParam)) {
             $arr["replace"] = [
-
-                "class" => \lo\core\widgets\admin\ReplaceInTreeButton::className(),
+                "class" => ReplaceInTreeButton::class,
                 "visible" => Yii::$app->user->can($this->access('update'), ['model' => $model]),
                 "label" => Yii::t('core', 'Replace'),
                 "options" => [
@@ -363,13 +280,8 @@ class Grid extends Widget
                     'class' => 'btn btn-primary',
                 ],
                 "route" => $this->baseRoute . "/replace",
-
             ];
-
         }
-
         return $arr;
-
     }
-
 }
