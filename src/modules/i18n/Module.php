b9653fbb6c4f82dc712af253cead107b0c68bfaf
Loveorigami <loveorigami@mail.ru>
2016-10-26 12:51:55 +0300

    missingTranslation

--- src/modules/i18n/Module.php
+++ src/modules/i18n/Module.php
@@ -2,6 +2,9 @@
 
 namespace lo\core\modules\i18n;
 
+use lo\core\modules\i18n\models\I18nSourceMessage;
+use yii\i18n\MissingTranslationEvent;
+
 class Module extends \yii\base\Module
 {
     public function init()
@@ -9,5 +12,29 @@ class Module extends \yii\base\Module
         parent::init();
 
         // custom initialization code goes here
+    }
+    /**
+     * @param MissingTranslationEvent $event
+     */
+    public static function missingTranslation(MissingTranslationEvent $event)
+    {
+        $sourceMessage = I18nSourceMessage::find()
+            ->where('category = :category and message = binary :message', [
+                ':category' => $event->category,
+                ':message' => $event->message
+            ])
+            ->with('messages')
+            ->one();
+
+        if (!$sourceMessage && $event->message) {
+            $sourceMessage = new I18nSourceMessage;
+            $sourceMessage->setAttributes([
+                'category' => $event->category,
+                'message' => $event->message
+            ], false);
+            $sourceMessage->save(false);
+        }
+        $sourceMessage->initI18nMessages();
+        $sourceMessage->saveI18nMessages();
     }
 }
