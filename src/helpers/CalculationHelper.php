4bfb77d1acb0fde466082a6c7ef3c6b5e7b9db97
Loveorigami <loveorigami@mail.ru>
2016-10-31 13:52:10 +0300

    calculation helper

---
+++ src/helpers/CalculationHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace lo\core\helpers;
+
+/**
+ * Class CalculationHelper
+ * @package lo\core\helpers
+ * @author Lukyanov Andrey <loveorigami@mail.ru>
+ */
+class CalculationHelper
+{
+    /**
+     * @param $value
+     * @param $total
+     * @return int
+     */
+    public static function getPersent($value, $total)
+    {
+        if ($total < 0) return 0;
+        if ($total < $value) return 100;
+
+        return ($total - $value) / $total;
+    }
+
+}
