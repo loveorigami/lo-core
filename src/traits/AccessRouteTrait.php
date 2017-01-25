323ad8c24235696e82b60b7e0b2aaab48a81dcbf
LoveOrigami <loveorigami@mail.ru>
2017-01-25 12:41:58 +0300

    render access

--- src/traits/AccessRouteTrait.php
+++ src/traits/AccessRouteTrait.php
@@ -26,7 +26,7 @@ trait AccessRouteTrait
      */
     public function getBaseRoute()
     {
-        if ($this->_baseRoute === null){
+        if ($this->_baseRoute === null) {
             $context = $this->view->context;
             /** @var Controller $context */
             $this->_baseRoute = "/" . $context->uniqueId;
@@ -61,5 +61,35 @@ trait AccessRouteTrait
 
         return $this->getPermRoute();
     }
+
+    /**
+     * @param $description
+     * @param $rule
+     * @param array $params
+     * @return string
+     */
+    /*
+    protected function renderAccess($description, $rule, $params = [])
+       {
+           $access = Yii::$app->user->can($rule, $params);
+           return $description.': '.($access ? 'yes' : 'no')      }
+
+   /*    public function actionTest()
+       {
+           $post = new stdClass();
+           $post->createdBy = User::findByUsername('demo')->id;
+           return $this->renderContent(
+               Html::tag('h1', 'Current permissions') .
+               Html::ul([
+                   $this->renderAccess('Use can create post', 'createPost'),
+                   $this->renderAccess('Use can read post', 'readPost'),
+                   $this->renderAccess('Use can update post', 'updatePost'),
+                   $this->renderAccess('Use can own update post', 'updateOwnPost', [
+                       'post' => $post,
+                   ]),
+                   $this->renderAccess('Use can delete post', 'deletePost'),
+               ])
+           );
+       }*/
 
 }
