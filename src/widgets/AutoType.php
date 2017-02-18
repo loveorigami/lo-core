bd87d6eef740a1620c92f130df2716fce3e2f1c7
Loveorigami <loveorigami@mail.ru>
2017-02-18 11:28:44 +0300

    AutoType

---
+++ src/widgets/AutoType.php
@@ -0,0 +1,14 @@
+<?php
+namespace lo\core\widgets;
+
+use kartik\typeahead\Typeahead;
+
+/**
+ * Class AutoType
+ * @package lo\core\widgets
+ * @author Lukyanov Andrey <loveorigami@mail.ru>
+ */
+class AutoType extends Typeahead
+{
+
+}
