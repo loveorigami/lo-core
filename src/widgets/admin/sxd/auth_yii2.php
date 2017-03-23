db1712f254d81bd4cce77d1823e602a9fb4cf9a2
Loveorigami <loveorigami@mail.ru>
2017-03-23 10:32:05 +0300

    sxd fix

--- src/widgets/admin/sxd/auth_yii2.php
+++ src/widgets/admin/sxd/auth_yii2.php
@@ -11,10 +11,10 @@ require(__DIR__ . '/../../../../common/config/bootstrap.php');
 require(__DIR__ . '/../../../../backend/config/bootstrap.php');
 
 $config = \yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../../../common/config/main.global.php'),
-    require(__DIR__ . '/../../../../common/config/main.local.php'),
-    require(__DIR__ . '/../../../../backend/config/main.global.php'),
-    require(__DIR__ . '/../../../../backend/config/main.local.php')
+    require(__DIR__ . '/../../../../common/config/main.php'),
+    require(__DIR__ . '/../../../../common/config/main-local.php'),
+    require(__DIR__ . '/../../../../backend/config/main.php'),
+    require(__DIR__ . '/../../../../backend/config/main-local.php')
 );
 
 $application = new \yii\web\Application($config);
