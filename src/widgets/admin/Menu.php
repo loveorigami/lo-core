f6580be98a694346cf564ca0dcc966c2d5827e34
Loveorigami <loveorigami@mail.ru>
2017-11-07 10:04:55 +0300

    upd menu

--- src/widgets/admin/Menu.php
+++ src/widgets/admin/Menu.php
@@ -12,47 +12,48 @@ use yii\helpers\Url;
  */
 class Menu extends \yii\widgets\Menu
 {
-    /**
-     * @var string
-     */
+    /** @var string */
     public $linkTemplate = "<a href=\"{url}\">\n{icon}\n{label}\n{right-icon}\n{badge}</a>";
 
-    /**
-     * @var string
-     */
-    public $labelTemplate = '{icon}\n{label}\n{badge}';
+    /** @var string */
+    public $labelTemplate = '{icon}\n{label}';
+    public $submenuTemplate = "\n<ul class='treeview-menu' {show}>\n{items}\n</ul>\n";
+    public $activateParents = true;
+    public $defaultIconHtml = '<i class="fa fa-circle-o"></i> ';
 
-    /**
-     * @var string
-     */
-    public $badgeTag = 'span';
-
-    /**
-     * @var string
-     */
-    public $badgeClass = 'label pull-right';
-
-    /**
-     * @var string
-     */
-    public $badgeBgClass;
-
-    /**
-     * @var string
-     */
+    /** @var string */
     public $parentRightIcon = '<i class="fa fa-angle-left pull-right"></i>';
+
+
+    /**
+     * @inheritdoc
+     */
+    protected function renderItem2($item)
+    {
+        if (isset($item['items'])) {
+            $labelTemplate = '<a href="{url}">{icon} {label} <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>';
+            $linkTemplate = '<a href="{url}">{icon} {label} <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>';
+        } else {
+            $labelTemplate = $this->labelTemplate;
+            $linkTemplate = $this->linkTemplate;
+        }
+
+        $replacements = [
+            '{label}' => strtr($this->labelTemplate, ['{label}' => $item['label'],]),
+            '{icon}' => empty($item['icon']) ? $this->defaultIconHtml
+                : '<i class="' . self::$iconClassPrefix . $item['icon'] . '"></i> ',
+            '{url}' => isset($item['url']) ? Url::to($item['url']) : 'javascript:void(0);',
+        ];
+
+        $template = ArrayHelper::getValue($item, 'template', isset($item['url']) ? $linkTemplate : $labelTemplate);
+        return strtr($template, $replacements);
+    }
 
     /**
      * @inheritdoc
      */
     protected function renderItem($item)
     {
-        $item['badgeOptions'] = isset($item['badgeOptions']) ? $item['badgeOptions'] : [];
-
-        if (!ArrayHelper::getValue($item, 'badgeOptions.class')) {
-            $bg = isset($item['badgeBgClass']) ? $item['badgeBgClass'] : $this->badgeBgClass;
-            $item['badgeOptions']['class'] = $this->badgeClass . ' ' . $bg;
-        }
 
         if (isset($item['items']) && !isset($item['right-icon'])) {
             $item['right-icon'] = $this->parentRightIcon;
@@ -62,9 +63,6 @@ class Menu extends \yii\widgets\Menu
             $template = ArrayHelper::getValue($item, 'template', $this->linkTemplate);
 
             return strtr($template, [
-                '{badge}' => isset($item['badge'])
-                    ? Html::tag('small', $item['badge'], $item['badgeOptions'])
-                    : '',
                 '{icon}' => isset($item['icon']) ? $item['icon'] : '',
                 '{right-icon}' => isset($item['right-icon']) ? $item['right-icon'] : '',
                 '{url}' => Url::to($item['url']),
@@ -72,16 +70,58 @@ class Menu extends \yii\widgets\Menu
             ]);
         } else {
             $template = ArrayHelper::getValue($item, 'template', $this->labelTemplate);
-
             return strtr($template, [
-                '{badge}' => isset($item['badge'])
-                    ? Html::tag('small', $item['badge'], $item['badgeOptions'])
-                    : '',
                 '{icon}' => isset($item['icon']) ? $item['icon'] : '',
                 '{right-icon}' => isset($item['right-icon']) ? $item['right-icon'] : '',
                 '{label}' => $item['label'],
             ]);
         }
+    }
+
+    /**
+     * Recursively renders the menu items (without the container tag).
+     * @param array $items the menu items to be rendered recursively
+     * @return string the rendering result
+     */
+    protected function renderItems($items)
+    {
+        $n = count($items);
+        $lines = [];
+        foreach ($items as $i => $item) {
+            $options = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
+            $tag = ArrayHelper::remove($options, 'tag', 'li');
+            $class = [];
+            if ($item['active']) {
+                $class[] = $this->activeCssClass;
+            }
+            if ($i === 0 && $this->firstItemCssClass !== null) {
+                $class[] = $this->firstItemCssClass;
+            }
+            if ($i === $n - 1 && $this->lastItemCssClass !== null) {
+                $class[] = $this->lastItemCssClass;
+            }
+            if (!empty($class)) {
+                if (empty($options['class'])) {
+                    $options['class'] = implode(' ', $class);
+                } else {
+                    $options['class'] .= ' ' . implode(' ', $class);
+                }
+            }
+            $menu = $this->renderItem($item);
+            if (!empty($item['items'])) {
+                $menu .= strtr($this->submenuTemplate, [
+                    '{show}' => $item['active'] ? "style='display: block'" : '',
+                    '{items}' => $this->renderItems($item['items']),
+                ]);
+                if (isset($options['class'])) {
+                    $options['class'] .= ' treeview';
+                } else {
+                    $options['class'] = 'treeview';
+                }
+            }
+            $lines[] = Html::tag($tag, $menu, $options);
+        }
+        return implode("\n", $lines);
     }
 
     /**
