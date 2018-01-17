bd584b9b6e9b31948e548d812d6a0a83085f8eae
Loveorigami <loveorigami@mail.ru>
2018-01-17 20:05:22 +0300

    utf8 clear

--- src/helpers/StringHelper.php
+++ src/helpers/StringHelper.php
@@ -130,6 +130,16 @@ class StringHelper extends YiiStringHelper
     }
 
     /**
+     * @param $str
+     * @return string
+     * @see https://stackoverflow.com/questions/35961245/how-to-remove-all-emoji-from-string-php
+     */
+    public static function clearNonUtf8($str)
+    {
+        return preg_replace('/([0-9|#][\x{20E3}])|[\x{00ae}|\x{00a9}|\x{203C}|\x{2047}|\x{2048}|\x{2049}|\x{3030}|\x{303D}|\x{2139}|\x{2122}|\x{3297}|\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $str);
+    }
+
+    /**
      * @param $str - the input string.
      * @return string
      */
