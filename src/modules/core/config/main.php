adf2b7222ae8d7f366d878c3241afbcdd28611dc
Lukyanov Andrey <loveorigami@mail.ru>
2019-02-07 13:50:42 +0300

    move sxd config

--- src/modules/core/config/main.php
+++ src/modules/core/config/main.php
@@ -4,8 +4,29 @@ return [
     'modules' => [
         'core' => [
             'class' => 'lo\core\modules\core\Module',
-			'controllerNamespace' => 'lo\core\modules\core\controllers',
+            'controllerNamespace' => 'lo\core\modules\core\controllers',
             'defaultRoute' => 'sypex-dumper',
+        ],
+        'elfinder' => [
+            'controllerMap' => [
+                'sxd' => [
+                    'class' => lo\modules\elfinder\controllers\PathController::class,
+                    'access' => ['root'],
+                    'root' => [
+                        'baseUrl' => '', // /uploads
+                        'basePath' => '@sxdPath', // site.lo/uploads
+                        'path' => '',
+                        'access' => ['read' => '*', 'write' => 'root'],
+                        'name' => ['category' => 'backend', 'message' => 'Category'],
+                        'driver' => 'LocalFileSystem',
+                        'options' => [
+                            'tmbSize' => '48',
+                            'acceptedName' => '/^[0-9a-z_\-.]+$/i', // i любой регистр только англ
+                            'imgLib' => 'gd',
+                        ],
+                    ],
+                ],
+            ],
         ],
     ],
 ];
