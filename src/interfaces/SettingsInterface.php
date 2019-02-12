639a5d7f17be8d9b46d9eac81f74b40ba2d568ff
Lukyanov Andrey <loveorigami@mail.ru>
2019-02-12 14:12:53 +0300

    add interface

---
+++ src/interfaces/SettingsInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace lo\core\interfaces;
+
+/**
+ * Interface SettingsInterface
+ *
+ * @package lo\core\modules\settings\components
+ */
+interface SettingsInterface
+{
+    /**
+     * @param $key
+     * @return string;
+     */
+    public function get($key): ?string;
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function set($key, $value): void;
+}
