5e5942b9e5ef13edd6f5979ddff44fd0868bbd78
Lukyanov Andrey <loveorigami@mail.ru>
2019-06-05 17:09:55 +0300

    json field

--- src/behaviors/JsonBehavior.php
+++ src/behaviors/JsonBehavior.php
@@ -13,57 +13,11 @@ use yii\db\ActiveRecord;
 use yii\helpers\Json;
 
 /**
- * Class JsonBehavior.
- * Behavior for encoding and decoding model fields as JSON.
+ * Class JsonBehavior
  *
- * ```php
- *
- * public function behaviors()
- * {
- *     return [
- *          [
- *               'class' => JsonBehavior::className(),
- *               'fields' => ['name_of_field#1', 'name_of_field#2',],
- *          ],
- *     ];
- * }
- *
- * @author    Serhiy Vinichuk <serhiyvinichuk@gmail.com>
- * @copyright 2014 NullReferenceException
- * @license   MIT
+ * @package lo\core\behaviors
  */
-class JsonBehavior extends Behavior
+class JsonBehavior extends \paulzi\jsonBehavior\JsonBehavior
 {
-    public $fields = [];
 
-    public $default = [];
-
-    public function events()
-    {
-        return [
-            ActiveRecord::EVENT_BEFORE_INSERT => 'encode',
-            ActiveRecord::EVENT_BEFORE_UPDATE => 'encode',
-            ActiveRecord::EVENT_AFTER_FIND => 'decode',
-            ActiveRecord::EVENT_AFTER_INSERT => 'decode',
-            ActiveRecord::EVENT_AFTER_UPDATE => 'decode',
-        ];
-    }
-
-    public function encode()
-    {
-        $model = $this->owner;
-        foreach ($this->fields as $field) {
-            if (isset($model->$field)) {
-                $model->$field = Json::encode($model->$field);
-            }
-        }
-    }
-
-    public function decode()
-    {
-        $model = $this->owner;
-        foreach ($this->fields as $field) {
-            $model->$field = empty($model->$field) ? $this->default : Json::decode($model->$field);
-        }
-    }
 }
---
+++ src/db/fields/JsonField.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace lo\core\db\fields;
+
+use lo\core\behaviors\JsonBehavior;
+use lo\core\db\ActiveRecord;
+use lo\core\inputs\ReadOnlyInput;
+use lo\core\inputs\TextInput;
+use lo\core\inputs\TranslitInput;
+use yii\helpers\ArrayHelper;
+use Zelenin\yii\behaviors\Slug;
+
+/**
+ * Class JsonField
+ *
+ * @package lo\core\db\fields
+ */
+class JsonField extends TextField
+{
+    /** Преффикс поведения */
+    const BEHAVIOR_PREF = "json";
+
+    /** @var bool */
+    public $isRequired = false;
+
+    /** @var string */
+    public $filterInputClass = TextInput::class;
+
+    /** @var array настройки поведения генерации uuid */
+    public $jsonOptions = [];
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        $parent = parent::behaviors();
+
+        if ($this->model->scenario != ActiveRecord::SCENARIO_SEARCH) {
+
+            $code = self::BEHAVIOR_PREF . ucfirst($this->attr);
+
+            $parent[$code] = ArrayHelper::merge([
+                'class' => JsonBehavior::class,
+                'attributes' => [$this->attr],
+            ], $this->jsonOptions);
+        }
+
+        return $parent;
+    }
+
+    /**
+     * Правила валидации
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = parent::rules();
+
+        $rules[] = array_merge([$this->attr, JsonValidator::class, 'except' => ActiveRecord::SCENARIO_SEARCH]);
+
+        return $rules;
+    }
+}
