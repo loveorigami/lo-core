1bdf75fe72a879490bb1f33ae59edee9af536185
Lukyanov Andrey <loveorigami@mail.ru>
2019-09-11 15:42:44 +0300

    add color

--- src/db/fields/ColorField.php
+++ src/db/fields/ColorField.php
@@ -3,6 +3,7 @@
 namespace lo\core\db\fields;
 
 use lo\core\db\ActiveQuery;
+use lo\core\helpers\Html;
 use lo\core\inputs\ColorInput;
 
 /**
@@ -13,4 +14,48 @@ use lo\core\inputs\ColorInput;
 class ColorField extends TextField
 {
     public $inputClass = ColorInput::class;
+
+    /**
+     * Конфигурация поля для грида (GridView)
+     *
+     * @return array
+     */
+    protected function grid()
+    {
+        $grid = $this->defaultGrid();
+
+        $grid["format"] = 'raw';
+        $grid['headerOptions'] = [
+            'style' => 'width: 50 px;',
+        ];
+        $grid["value"] = function ($model) {
+            return $this->getGridValue($model);
+        };
+
+        return $grid;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultGridFilter()
+    {
+        return false;
+    }
+
+    /**
+     * Вывод значения в гриде
+     *
+     * @param $model
+     * @return string
+     */
+    protected function getGridValue($model)
+    {
+        $value = $model->{$this->attr} ?: '#999';
+
+        return Html::tag('span', $value, [
+            'class' => 'label',
+            'style' => 'background-color:' . $value,
+        ]);
+    }
 }
